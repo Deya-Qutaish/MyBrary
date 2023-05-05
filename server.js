f7144caf3433200ae3830d658782fa398c8f8966
Deya-Qutaish <dia.qutash4@gmail.com>
2023-05-05 13:22:26 +0300

    Initial Author Route

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
 
 // requiring the index route file so our server knows it exists to use it
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
 
 // Setting up view engine and layouts and views for our page rendering
 app.set("view engine", "ejs");
@@ -16,6 +18,7 @@ app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 // importing mongoose
 const mongoose = require("mongoose");
@@ -26,6 +29,7 @@ db.once("open", () => console.log("Connected to Mongoose"));
 
 // telling our app to handle the indexRouter
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 // Listening in on ports from our server
 app.listen(process.env.PORT || 3000);
